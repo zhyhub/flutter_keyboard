7287b7b31fc4f27b6029b70d2d5383d5b622ef25
sushi1015 <sushi1015@icloud.com>
2019-02-26 12:57:49 +0800

    优化输入框

--- lib/keyboard/sushi_input_widget.dart
+++ lib/keyboard/sushi_input_widget.dart
@@ -24,68 +24,43 @@ class _SushiPainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    print('paint ======== ${size.width}');
     _rectPaint = Paint()
-      ..color = Color(0xff707070)
-      ..style = PaintingStyle.stroke
+      ..color = Colors.blue
+      ..style = PaintingStyle.fill
       ..strokeWidth = 2
       ..isAntiAlias = true;
 
-    double _width = (size.width - ((pinLength - 1) * 16)) / pinLength;
-
-    double startX = 0.0;
-    double startY = _width + 10;
+    double _x = size.width / 7 / 2;
+    double _y = size.width / 7 / 2;
+    double _textSize = 18.0;
 
     for (int i = 0; i < pinLength; i++) {
-      RRect rRect = RRect.fromRectAndRadius(
-          Rect.fromLTRB(
-            startX,
-            0,
-            startX + startY,
-            startY,
-          ),
-          Radius.circular(0.0));
+      Rect rect = Rect.fromCircle(center: Offset(_x, _y), radius: _y);
+      RRect rRect = RRect.fromRectAndRadius(rect, Radius.circular(0.0));
       canvas.drawRRect(rRect, _rectPaint);
-      if (i == pinLength - 1) {
-        startX = size.width;
-      } else {
-        startX += _width + 16;
-      }
-      print('fromRectAndRadius ======== $startX  ,  $startY  , $_width');
+      _x = _x + size.width / 7;
     }
 
-    var index = 0;
-    startY = 0.0;
+    _x = size.width / 7 / 2;
 
     text.runes.forEach((rune) {
       String code = String.fromCharCode(rune);
-      TextPainter textPainter = TextPainter(
-        text: TextSpan(
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24.0,
+      TextPainter(
+          text: TextSpan(
+            text: code,
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: _textSize,
+                textBaseline: TextBaseline.ideographic,
+                fontWeight: FontWeight.bold),
           ),
-          text: code,
-        ),
-        textAlign: TextAlign.center,
-        textDirection: TextDirection.ltr,
-      );
-      textPainter.layout();
+          textDirection: TextDirection.ltr,
+          textAlign: TextAlign.center)
+        ..layout(maxWidth: 0.0, minWidth: 0.0)
+        ..paint(canvas, Offset(_x, _y / 2));
 
-      if (startY == 0.0) {
-        startY = _width / 2 - textPainter.height / 2 + 6;
-      }
-      startX = _width * index +
-          _width / 2 -
-          textPainter.width / 2 +
-          16 * index +
-          index * 2;
-
-      textPainter.paint(canvas, Offset(startX, startY));
-      index++;
-
-      print(
-          'TextPainter ======== $startX  ,  $startY , $index  ， =======   ${textPainter.height}   ....   ${textPainter.width}');
+      _x = _x + size.width / 7;
+      _textSize = 24;
     });
   }
 
