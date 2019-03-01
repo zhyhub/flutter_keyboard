f5160f00e877e406eb0ed76b8144c765bf4b0916
sushi1015 <sushi1015@icloud.com>
2019-03-01 15:39:15 +0800

    优化汉字居中问题

--- lib/keyboard/sushi_input_widget.dart
+++ lib/keyboard/sushi_input_widget.dart
@@ -22,6 +22,8 @@ class _SushiPainter extends CustomPainter {
 
   int pinLength = 7;
 
+  double _textSize = 24.0;
+
   @override
   void paint(Canvas canvas, Size size) {
     _rectPaint = Paint()
@@ -30,37 +32,38 @@ class _SushiPainter extends CustomPainter {
       ..strokeWidth = 2
       ..isAntiAlias = true;
 
-    double _x = size.width / 7 / 2;
-    double _y = size.width / 7 / 2;
-    double _textSize = 18.0;
+    double _baseWidth = size.width / pinLength;
+
+    double _x = _baseWidth / 2;
+    double _y = _baseWidth / 2;
 
     for (int i = 0; i < pinLength; i++) {
       Rect rect = Rect.fromCircle(center: Offset(_x, _y), radius: _y);
       RRect rRect = RRect.fromRectAndRadius(rect, Radius.circular(0.0));
       canvas.drawRRect(rRect, _rectPaint);
-      _x = _x + size.width / 7;
+      _x = _x + _baseWidth;
     }
 
-    _x = size.width / 7 / 2;
+    _x = _baseWidth / 2;
 
     text.runes.forEach((rune) {
       String code = String.fromCharCode(rune);
-      TextPainter(
+      TextPainter textPainter = TextPainter(
           text: TextSpan(
             text: code,
             style: TextStyle(
                 color: Colors.white,
                 fontSize: _textSize,
                 textBaseline: TextBaseline.ideographic,
-                fontWeight: FontWeight.bold),
+                fontWeight: FontWeight.normal),
           ),
           textDirection: TextDirection.ltr,
-          textAlign: TextAlign.center)
-        ..layout(maxWidth: 0.0, minWidth: 0.0)
-        ..paint(canvas, Offset(_x, _y / 2));
+          textAlign: TextAlign.center);
+      textPainter.layout(maxWidth: 0.0, minWidth: 0.0);
+      textPainter.paint(canvas,
+          Offset(_x - textPainter.width / 2, _y - textPainter.height / 2));
 
-      _x = _x + size.width / 7;
-      _textSize = 24;
+      _x = _x + _baseWidth;
     });
   }
 
